370f48328d99796a6349c6f0e2e75e4e1aac3bb1
BrunoFerreira95 <brunoferreirapedraca@gmail.com>
2020-01-15 10:15:32 -0300

    Construindo a intercade web 1/2

---
+++ web/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import './global.css';
+import './app.css';
+import './Sidebar.css';
+// Componente: é um bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
+function App() {
+
+  
+  return (
+   <div id="app">
+     <aside>
+      <strong>Cadastrar</strong>
+      <form>
+        <div class="input-block">
+          <label htmlFor="github_username">Usuário do Github</label>
+          <input name="github_username" id="github_username" required/>
+        </div>
+
+        <div class="input-block">
+          <label htmlFor="techs">Tecnologias</label>
+          <input name="techs" id="techs" required/>
+        </div>
+
+        <div className="input-group">
+          <div class="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input name="latitude" id="latitude" required/>
+          </div>
+        
+          <div class="input-block">
+            <label htmlFor="longitude">Longitude</label>
+            <input name="longitude" id="longitude" required/>
+          </div>
+        </div>
+        <button type="submit">Salvar</button>
+      </form>
+     </aside>
+     <main>
+
+     </main>
+   </div>
+  );
+}
+
+export default App;

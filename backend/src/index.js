cb8e131feed5e38110143cf8168b8523ff126c61
BrunoFerreira95 <brunoferreirapedraca@gmail.com>
2020-01-16 00:52:36 -0300

    Construindo a in terface web 2/2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,8 @@ mongoose.connect('mongodb+srv://omnistack:123@bruno@cluster0-jalnn.mongodb.net/w
     useUnifiedTopology: true,
     useCreateIndex: true
 });
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,46 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './app.css';
 import './Sidebar.css';
+import './Main.css';
+
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 // Componente: é um bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
 // Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
+  const [devs, setDevs] = useState([]);
 
   
+
+  
+  
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data])
+  }
+
   return (
    <div id="app">
      <aside>
       <strong>Cadastrar</strong>
-      <form>
-        <div class="input-block">
-          <label htmlFor="github_username">Usuário do Github</label>
-          <input name="github_username" id="github_username" required/>
-        </div>
-
-        <div class="input-block">
-          <label htmlFor="techs">Tecnologias</label>
-          <input name="techs" id="techs" required/>
-        </div>
-
-        <div className="input-group">
-          <div class="input-block">
-            <label htmlFor="latitude">Latitude</label>
-            <input name="latitude" id="latitude" required/>
-          </div>
-        
-          <div class="input-block">
-            <label htmlFor="longitude">Longitude</label>
-            <input name="longitude" id="longitude" required/>
-          </div>
-        </div>
-        <button type="submit">Salvar</button>
-      </form>
+        <DevForm onSubmit={handleAddDev}/>
      </aside>
      <main>
-
+      <ul>
+        {devs.map( dev => (
+          <DevItem key={dev._id} dev={dev} />
+        ))}
+        
+      </ul>
      </main>
    </div>
   );
